3769027ee0f7e9897bbdca266a9d5e740c9a61b8
root <root@DESKTOP-VV76MO9.localdomain>
2022-05-19 18:21:33 +1000

    First commit with flutter starter application. Started setting colors and themes.

---
+++ android/app/src/main/kotlin/com/example/besitary/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.besitary
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const kprimary = Color(0xFF212121);
+const kprimaryLight = Color(0xFF484848);
+const kprimaryDark = Color(0xFF000000);
+
+const ksecondary = Color(0xFFE0E0E0);
+const ksecondaryLight = Color(0xFFFFFFFF);
+const ksecondaryDark = Color(0xFFAEAEAE);
+
+const ktextonP = Color(0xFFFFFFFF);
+const ktextonS = Color(0xFF000000);
