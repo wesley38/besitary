e60e07476c8c79b1723fba2c0995b09945125f5f
Wesley <wesleytruong38@gmail.com>
2022-05-19 18:35:24 +1000

    Changed appbar title and fixed up comments

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // Building application themes.
     final IconThemeData _icontheme = IconThemeData(
       color: ktextonP,
     );
@@ -28,15 +29,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primaryColor: kprimary,
         primaryColorDark: kprimaryDark,
         primaryColorLight: kprimaryLight,
@@ -91,9 +83,7 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Besitary"),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
